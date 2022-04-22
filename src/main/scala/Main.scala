221de8f46662f36ae0425f2ac774be5b402bc0ae
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2022-04-22 13:07:56 +0200

    feat: handle multiple repositories at the same time

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,38 @@
 import dependencies.Python
-import scala.util.Success
-import scala.util.Failure
+import scala.util.{Success, Failure}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
+import scala.concurrent.Future
 
 @main def hello: Unit =
-  val filename =
-    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-core/requirements.txt"
+  val repoPaths = List(
+    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-core",
+    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-lynx",
+    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-delegator",
+    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-pydentifier"
+  )
 
-  val fileContents = Source.fromFile(filename).getLines.mkString("\n")
-  Python
-    .getDependencies(fileContents, Python.Pypi.getLatestVersion)
-    .onComplete {
-      case Success(dependencies) => dependencies.foreach(println)
-      case Failure(err)  => println(s"error = $err")
-    }
+  def appendReqsFile(path: String): String = s"$path/requirements.txt"
+  def getFileContents(path: String): String =
+    Source.fromFile(path).getLines.mkString("\n")
+
+  val getRepositoryDependencies = appendReqsFile
+    .andThen(getFileContents)
+    .andThen(fileContents =>
+      Python.getDependencies(fileContents, Python.Pypi.getLatestVersion)
+    )
+
+  val result =
+    Future.sequence(repoPaths.map(x => Future { getRepositoryDependencies(x) }))
+  result.onComplete {
+    case Success(repoDependencies) =>
+      repoDependencies.map(_.foreach { dependencies =>
+        {
+          println("*" * 10)
+          dependencies.foreach(println)
+        }
+      })
+    case Failure(err) => println(s"error = $err")
+  }
+
   Thread.sleep(100000)
