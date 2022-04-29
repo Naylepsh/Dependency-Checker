5708bd1fd290e7acd7fc9723b2185570b1e7cf40
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2022-04-29 20:47:23 +0200

    add basic version comparator

--- src/main/scala/Dependencies/package.scala
+++ src/main/scala/Dependencies/package.scala
@@ -1,3 +1,6 @@
+import scala.util.Try
+import scala.util.Success
+
 package object Dependencies {
   case class Dependency(
       name: String,
@@ -9,4 +12,47 @@ package object Dependencies {
       name: String,
       dependencies: List[Dependency]
   )
+
+  // TBD: Move this to a separate package?
+  enum VersionDifference:
+    case Patch, Minor, Major
+
+  def calculate_version_difference(
+      a: String,
+      b: String
+  ): Try[Option[VersionDifference]] = {
+    if (a == b)
+      return Success(None)
+
+    val version = extractVersion(a).flatMap { case (aMajor, aMinor, aPatch) =>
+      extractVersion(b).flatMap {
+        case (bMajor, bMinor, bPatch) => {
+          if (aMajor != bMajor)
+            Some(VersionDifference.Major)
+          else if (aMinor != bMinor)
+            Some(VersionDifference.Minor)
+          else if (aPatch != bPatch)
+            Some(VersionDifference.Patch)
+          else
+            None
+        }
+      }
+    }
+    Success(version)
+  }
+
+  private def extractVersion(text: String): Option[(String, String, String)] = {
+    versionPattern
+      .findFirstMatchIn(text)
+      .map(matches => {
+        val major = matches.group(1)
+        val minor = matches.group(2)
+        val patch = matches.group(3)
+
+        (major, minor, patch)
+      })
+  }
+
+  private val versionPattern = "[~^]*([0-9])+.([0-9])+.([0-9])+".r
+
 }
---
+++ src/test/scala/Dependencies/package.scala
@@ -0,0 +1,44 @@
+package Dependencies
+
+import org.scalatest._
+import org.scalatest.OptionValues.convertOptionToValuable
+import org.scalatest.TryValues.convertTryToSuccessOrFailure
+import flatspec._
+import matchers._
+import scala.util.Success
+import scala.util.Try
+
+class PackageSpec extends AnyFlatSpec with should.Matchers {
+  "calculate version difference" should "detect major differences" in {
+    calculate_version_difference(
+      "1.2.3",
+      "2.2.3"
+    ).success.value shouldBe Some(VersionDifference.Major)
+  }
+
+  it should "detect minor differences" in {
+    calculate_version_difference(
+      "1.2.3",
+      "1.3.3"
+    ).success.value shouldBe Some(VersionDifference.Minor)
+  }
+  it should "detect patch differences" in {
+    calculate_version_difference(
+      "1.2.3",
+      "1.2.4"
+    ).success.value shouldBe Some(VersionDifference.Patch)
+  }
+  it should "detect when there are no differences" in {
+    calculate_version_difference("1.2.3", "1.2.3").success.value shouldBe None
+  }
+  it should "have no result on non-purely-numeric versions" in {
+    calculate_version_difference("1.2b.3", "1.2.3").success.value shouldBe None
+  }
+  it should "handle semantic versioning symbols" in {
+    // TODO: take symbols into context,
+    // when ^ and minor doesn't match, then no difference
+    // when ~ and patch doesn't match, then no difference
+    calculate_version_difference("^1.2.3", "1.2.3").success.value shouldBe None
+    // calculate_version_difference("~1.2b.3", "1.2.3").success.value shouldBe None
+  }
+}
