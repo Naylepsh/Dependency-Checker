a89b65d62befee7ef1fec4ffd0afd16664614d9e
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2022-05-01 09:12:06 +0200

    cleanup: remove legacy local readLocal

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -16,37 +16,6 @@ import Dependencies.RepositoryDependencies
 import Dependencies.Excel
 import Dependencies.RepositoryDependenciesSheetExporter
 
-def readLocal: Unit =
-  val repoPaths = List(
-    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-core",
-    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-lynx",
-    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-delegator",
-    "/Users/sebastian.kondraciuk/Documents/code/vsa/vsa-pydentifier"
-  )
-
-  def appendReqsFile(path: String): String = s"$path/requirements.txt"
-  def getLocalFileContents(path: String): String =
-    Source.fromFile(path).getLines.mkString("\n")
-
-  val getDeps = Python.getDependencies(
-    x => Future { appendReqsFile.andThen(getLocalFileContents)(x) },
-    Python.Pypi.getLatestVersion
-  )
-
-  val dependenciesFuture =
-    Future.sequence(repoPaths.map(path => Future { (path, getDeps(path)) }))
-
-  val dependencies = Await.result(dependenciesFuture, Duration.Inf)
-  dependencies.map {
-    case (path, dependenciesFuture) => {
-      dependenciesFuture.map { dependencies =>
-        println("*" * 10)
-        println(path)
-        dependencies.foreach(println)
-      }
-    }
-  }
-
 @main def readRemote: Unit =
   import Data._
 
