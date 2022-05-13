967fd037545894bb34cc01aefe8303780f895ac2
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2022-05-13 22:21:13 +0200

    fix: leave blank color on excel export when a version cannot be parsed properly

--- src/main/scala/Dependencies/package.scala
+++ src/main/scala/Dependencies/package.scala
@@ -1,5 +1,6 @@
 import scala.util.Try
 import scala.util.Success
+import scala.util.Failure
 
 package object Dependencies {
   case class Dependency(
@@ -26,31 +27,28 @@ package object Dependencies {
     if (a == b)
       return Success(None)
 
-    val version = extractVersion(a).flatMap {
-      case (aSymbol, aMajor, aMinor, aPatch) =>
-        extractVersion(b).flatMap {
-          case (_, bMajor, bMinor, bPatch) => {
-            if (aMajor != bMajor)
-              Some(VersionDifference.Major)
-            else if (aMajor == bMajor && aSymbol == Some("^"))
-              None
-            else if (aMinor != bMinor)
-              Some(VersionDifference.Minor)
-            else if (aMinor == bMinor && aSymbol == Some("~"))
-              None
-            else if (aPatch != bPatch)
-              Some(VersionDifference.Patch)
-            else
-              None
-          }
-        }
+    for {
+      (aSymbol, aMajor, aMinor, aPatch) <- extractVersion(a)
+      (_, bMajor, bMinor, bPatch) <- extractVersion(b)
+    } yield {
+      if (aMajor != bMajor)
+        Some(VersionDifference.Major)
+      else if (aMajor == bMajor && aSymbol == Some("^"))
+        None
+      else if (aMinor != bMinor)
+        Some(VersionDifference.Minor)
+      else if (aMinor == bMinor && aSymbol == Some("~"))
+        None
+      else if (aPatch != bPatch)
+        Some(VersionDifference.Patch)
+      else
+        None
     }
-    Success(version)
   }
 
   private def extractVersion(
       text: String
-  ): Option[(Option[String], String, String, String)] = {
+  ): Try[(Option[String], String, String, String)] = {
     versionPattern
       .findFirstMatchIn(text)
       .map(matches => {
@@ -61,7 +59,11 @@ package object Dependencies {
         val patch = matches.group(4)
 
         (symbol, major, minor, patch)
-      })
+      }) match {
+      case Some(version) => Success(version)
+      case None =>
+        Failure(RuntimeException(s"Could not parse the version from $text"))
+    }
   }
 
   private val versionPattern = "([~^])*([0-9])+.([0-9])+.([0-9])+".r
