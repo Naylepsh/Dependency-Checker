d852692f187311ec66416d6295039408f7eb4e23
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2022-11-27 22:16:03 +0100

    tests: use fake logger

--- src/test/scala/services/sources/GitlabSourceSpec.scala
+++ src/test/scala/services/sources/GitlabSourceSpec.scala
@@ -7,15 +7,15 @@ import org.scalatest.matchers.should.Matchers._
 import org.scalactic.Explicitly._
 import cats._
 import cats.implicits._
+import org.legogroup.woof.{given, *}
+import org.legogroup.woof.Logger.StringLocal
 import services.GitlabApi
 import services.responses._
 import domain.dependency.Dependency
-import domain.registry.Project
-import domain.registry.DependencySource
-import domain.registry.Format
+import domain.registry._
 
 class GitlabSourceSpec extends AnyFlatSpec {
-  import GitlabSourceSpec._
+  import GitlabSourceSpec.{given, *}
 
   "Extract" should "return an empty list if failed to get the project's concrete file" in {
     GitlabSource
@@ -62,5 +62,16 @@ object GitlabSourceSpec {
         branch: String,
         filePath: String
     ): Id[Either[Throwable, RepositoryFile]] = fileResult
+  }
+
+  given Logger[Id] = new Logger[Id] {
+
+    // Theorically unsafe, but since it doesn't seem to get called then... who cares?
+    override val stringLocal: StringLocal[Id] = null
+
+    override def doLog(level: LogLevel, message: String)(using
+        LogInfo
+    ): Id[Unit] = ()
+
   }
 }
