efdca0fac8137b54bc7e1730b2bd0de3db44ea8c
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2022-11-28 22:32:13 +0100

    refactor: cleanup Pypi module

--- src/main/scala/services/reporters/python/Pypi.scala
+++ src/main/scala/services/reporters/python/Pypi.scala
@@ -6,49 +6,47 @@ import domain.dependency.Dependency
 import domain.dependency.DependencyDetails
 import utils.json
 
-object Pypi {
+object Pypi:
 
   case class PackageInfo(version: String)
-  object PackageInfo {
+  object PackageInfo:
     given RW[PackageInfo] = macroRW
-  }
 
-  case class PackageRelease(upload_time: String, requires_python: String) {
-    val uploadTime = upload_time
-    val requiresPython = requires_python
-  }
-  object PackageRelease {
+  case class PackageRelease(
+      @upickle.implicits.key("upload_time")
+      uploadTime: String,
+      @upickle.implicits.key("requires_python")
+      requiresPython: String
+  )
+  object PackageRelease:
     given RW[PackageRelease] = macroRW
-  }
 
   case class PackageVulnerability(id: String, details: String)
-  object PackageVulnerability {
+  object PackageVulnerability:
     given RW[PackageVulnerability] = macroRW
-  }
 
   case class PypiResponse(
       info: PackageInfo,
       releases: Map[String, List[PackageRelease]]
   )
-  object PypiResponse {
+  object PypiResponse:
     given RW[PypiResponse] = macroRW
-  }
 
   case class VulnerabilitiesResponse(
       vulnerabilities: List[PackageVulnerability]
   )
-  object VulnerabilitiesResponse {
+  object VulnerabilitiesResponse:
     given RW[VulnerabilitiesResponse] = macroRW
-  }
+
   def getDependencyDetails(dependency: Dependency): Try[DependencyDetails] =
-    getLatestDependencyInfo(dependency).flatMap(response => {
+    getLatestDependencyInfo(dependency).flatMap(response =>
       val latestVersion = response.info.version
-      val requiredPython = for {
+      val requiredPython = for
         release <- response.releases
           .get(latestVersion)
           .flatMap(_.headOption)
         value <- Option(release.requiresPython)
-      } yield value
+      yield value
 
       getVulnerabilities(dependency).map(vulnerabilities => {
         DependencyDetails(
@@ -59,8 +57,7 @@ object Pypi {
           requiredPython
         )
       })
-
-    })
+    )
 
   private def getLatestDependencyInfo(
       dependency: Dependency
@@ -73,19 +70,15 @@ object Pypi {
   private def getVulnerabilities(
       dependency: Dependency
   ): Try[List[PackageVulnerability]] = Try {
-    val resource = dependency.currentVersion match {
+    val resource = dependency.currentVersion match
       case Some(version) => s"${dependency.name}/${cleanupVersion(version)}"
       case None          => dependency.name
-    }
+
     val response =
       requests.get(s"https://pypi.org/pypi/$resource/json")
-    val parsedResponse =
-      json.parse[VulnerabilitiesResponse](response.text())
-
-    parsedResponse.vulnerabilities
+    json.parse[VulnerabilitiesResponse](response.text()).vulnerabilities
   }
 
   private def cleanupVersion(version: String): String =
     // This is a temporary hack, for ~/^ version shoud be bumped to the latest appropriate one
     version.replaceAll("[\\^~]", "")
-}
