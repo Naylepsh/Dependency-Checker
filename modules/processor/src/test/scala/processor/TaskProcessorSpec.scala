15d7f8c4eac28327dd51908136f011881f41169f
Naylepsh <naylepsh@gmail.com>
2023-08-13 22:16:08 +0200

    fix some concurrency stuff and add a tests for additional workers

--- modules/processor/src/test/scala/processor/TaskProcessorSpec.scala
+++ modules/processor/src/test/scala/processor/TaskProcessorSpec.scala
@@ -23,8 +23,27 @@ class TaskProcessorSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers:
         taskHandler.tasksExecuted.get.asserting(_ shouldBe 0)
           *> processor.add(task)
           // Need to sleep to give processor some time to process the task
-          *> IO.sleep(4.seconds) 
+          *> IO.sleep(4.seconds)
           *> taskHandler.tasksExecuted.get.asserting(_ shouldBe 1)
+
+  "Execute tasks on the additional workers" in:
+    SlowTestTaskHandler.make.flatMap: taskHandler =>
+      TaskProcessor.make(
+        taskHandler,
+        2,
+        // Kill the main worker after the first task just to ensure additional worker
+        taskHandler.tasksExecuted.get.map(_ == 1),
+        None
+      ).use: processor =>
+        taskHandler.tasksExecuted.get.asserting(_ shouldBe 0)
+          *> processor.add(task)
+          *> IO.sleep(1.second)
+          *> processor.add(task)
+          // Need to sleep to give processor some time to process the task
+          // 4 seconds should be enough for two workers
+          // but not enough for one
+          *> IO.sleep(6.seconds)
+          *> taskHandler.tasksExecuted.get.asserting(_ shouldBe 2)
 
 object TaskProcessorSpec:
   class TestTaskHandler(val tasksExecuted: Ref[IO, Int])
@@ -33,6 +52,14 @@ object TaskProcessorSpec:
   object TestTaskHandler:
     def make: IO[TestTaskHandler] = Ref.of[IO, Int](0).map: tasksExecuted =>
       new TestTaskHandler(tasksExecuted)
+
+  class SlowTestTaskHandler(tasksExecuted: Ref[IO, Int])
+      extends TestTaskHandler(tasksExecuted):
+    override def execute(task: Task): IO[Unit] =
+      IO.sleep(3.seconds) *> super.execute(task)
+  object SlowTestTaskHandler:
+    def make: IO[TestTaskHandler] = Ref.of[IO, Int](0).map: tasksExecuted =>
+      new SlowTestTaskHandler(tasksExecuted)
 
   val task = Task.ScanTask(ProjectScanConfig(
     "123",
