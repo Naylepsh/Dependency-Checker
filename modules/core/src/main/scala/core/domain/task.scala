d57358121210d4c556dcc87073bd29c159e1898d
Naylepsh <naylepsh@gmail.com>
2023-08-13 23:10:09 +0200

    make task processor generic

--- modules/core/src/main/scala/core/domain/task.scala
+++ modules/core/src/main/scala/core/domain/task.scala
@@ -3,11 +3,5 @@ package core.domain
 import core.domain.registry.ProjectScanConfig
 
 object task:
-  enum Task:
-    case ScanTask(config: ProjectScanConfig) extends Task
-
   trait TaskProcessor[F[_]]:
-    def add(task: Task): F[Unit]
-
-  trait TaskHandler[F[_]]:
-    def execute(task: Task): F[Unit]
+    def add(task: => F[Unit]): F[Unit]
--- modules/processor/src/main/scala/processor/TaskProcessor.scala
+++ modules/processor/src/main/scala/processor/TaskProcessor.scala
@@ -1,29 +1,30 @@
 package processor
 
-import core.domain.task.{ Task, TaskHandler, TaskProcessor }
+import core.domain.task.TaskProcessor
 import cats.effect.std.{ Queue, Supervisor }
 import cats.effect.{ Ref, Resource, Spawn, Temporal }
 import cats.Monad
 import cats.syntax.all.*
 import concurrent.duration.*
 
+private type Task[F[_]] = () => F[Unit]
+
 object TaskProcessor:
+
   def make[F[_]: Monad: Temporal](
-      handler: TaskHandler[F],
       maxWorkers: Int,
       stopPredicate: F[Boolean],
       mainWorkerSleepTime: Option[Duration]
   ): Resource[F, TaskProcessor[F]] =
     Supervisor[F](await = true).evalMap: supervisor =>
       for
-        queue          <- Queue.unbounded[F, Task]
+        queue          <- Queue.unbounded[F, Task[F]]
         tasksInProgess <- Ref.of[F, Int](0)
         mainWorker =
-          TaskWorker(queue, handler, tasksInProgess, mainWorkerSleepTime)
+          TaskWorker(queue, tasksInProgess, mainWorkerSleepTime)
         processor = TaskProcessor.make(
           supervisor,
           queue,
-          handler,
           tasksInProgess,
           maxWorkers
         )
@@ -34,34 +35,32 @@ object TaskProcessor:
 
   private def make[F[_]: Monad: Temporal](
       supervisor: Supervisor[F],
-      queue: Queue[F, Task],
-      handler: TaskHandler[F],
+      queue: Queue[F, Task[F]],
       tasksInProgress: Ref[F, Int],
       maxWorkers: Int
   ): TaskProcessor[F] = new:
-    def add(task: Task): F[Unit] =
+    def add(task: => F[Unit]): F[Unit] =
       for
-        _               <- queue.offer(task)
+        _               <- queue.offer(() => task)
         inProgressCount <- tasksInProgress.get
-        _ <-
+        _               <-
           /**
            * TODO: There's an edge case here:
-           *  If a user adds the jobs quickly: 
+           *  If a user adds the jobs quickly:
            *  `processor.add(task1) *> processor.add(task2) *> processor.add(task3)`
-           *  then there's a chance that inProgressCount will still be 0 
+           *  then there's a chance that inProgressCount will still be 0
            *  by the time of `add(task2)` and `add(task3)`
            *  preventing the additional workers from spawning
            */
           if 0 < inProgressCount && inProgressCount <= maxWorkers
           then
-            val worker = TaskWorker(queue, handler, tasksInProgress)
+            val worker = TaskWorker(queue, tasksInProgress)
             supervisor.supervise(worker.runOnce).void
           else Monad[F].unit
       yield ()
 
 private class TaskWorker[F[_]: Monad: Temporal](
-    queue: Queue[F, Task],
-    handler: TaskHandler[F],
+    queue: Queue[F, Task[F]],
     tasksInProgress: Ref[F, Int],
     sleepOnEmpty: Option[Duration] = None
 ):
@@ -73,5 +72,5 @@ private class TaskWorker[F[_]: Monad: Temporal](
           .getOrElse(Monad[F].unit)
       case Some(task) =>
         tasksInProgress.update(_ + 1)
-          *> handler.execute(task)
+          *> task()
           *> tasksInProgress.update(_ - 1)
