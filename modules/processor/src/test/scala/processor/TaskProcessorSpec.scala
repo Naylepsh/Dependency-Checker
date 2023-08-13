cef435ae81a540893f1624a1930f51a430280b7d
Naylepsh <naylepsh@gmail.com>
2023-08-13 23:10:09 +0200

    make task processor generic

--- modules/processor/src/test/scala/processor/TaskProcessorSpec.scala
+++ modules/processor/src/test/scala/processor/TaskProcessorSpec.scala
@@ -3,7 +3,7 @@ package processor
 import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.should.Matchers
 import cats.effect.{ IO, Ref }
-import core.domain.task.{ Task, TaskHandler, TaskProcessor }
+import core.domain.task.TaskProcessor
 import cats.effect.testing.scalatest.AsyncIOSpec
 import cats.syntax.all.*
 import core.domain.registry.ProjectScanConfig
@@ -13,58 +13,39 @@ class TaskProcessorSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers:
   import TaskProcessorSpec.*
 
   "Execute tasks on the main worker" in:
-    TestTaskHandler.make.flatMap: taskHandler =>
+    Ref[IO].of[Int](0).flatMap: taskCount =>
       TaskProcessor.make(
-        taskHandler,
         1,
-        taskHandler.tasksExecuted.get.map(_ > 0),
+        taskCount.get.map(_ > 0),
         None
       ).use: processor =>
-        taskHandler.tasksExecuted.get.asserting(_ shouldBe 0)
-          *> processor.add(task)
+        taskCount.get.asserting(_ shouldBe 0)
+          *> processor.add(incTaskCount(taskCount))
           // Need to sleep to give processor some time to process the task
           *> IO.sleep(4.seconds)
-          *> taskHandler.tasksExecuted.get.asserting(_ shouldBe 1)
+          *> taskCount.get.asserting(_ shouldBe 1)
 
   "Execute tasks on the additional workers" in:
-    SlowTestTaskHandler.make.flatMap: taskHandler =>
+    Ref[IO].of[Int](0).flatMap: taskCount =>
       TaskProcessor.make(
-        taskHandler,
         2,
         // Kill the main worker after the first task just to ensure additional worker
-        taskHandler.tasksExecuted.get.map(_ == 1),
+        taskCount.get.map(_ == 1),
         None
       ).use: processor =>
-        taskHandler.tasksExecuted.get.asserting(_ shouldBe 0)
-          *> processor.add(task)
+        taskCount.get.asserting(_ shouldBe 0)
+          *> processor.add(slowIncTaskCount(taskCount))
           *> IO.sleep(1.second)
-          *> processor.add(task)
+          *> processor.add(slowIncTaskCount(taskCount))
           // Need to sleep to give processor some time to process the task
           // 4 seconds should be enough for two workers
           // but not enough for one
           *> IO.sleep(6.seconds)
-          *> taskHandler.tasksExecuted.get.asserting(_ shouldBe 2)
+          *> taskCount.get.asserting(_ shouldBe 2)
 
 object TaskProcessorSpec:
-  class TestTaskHandler(val tasksExecuted: Ref[IO, Int])
-      extends TaskHandler[IO]:
-    def execute(task: Task): IO[Unit] = tasksExecuted.update(_ + 1)
-  object TestTaskHandler:
-    def make: IO[TestTaskHandler] = Ref.of[IO, Int](0).map: tasksExecuted =>
-      new TestTaskHandler(tasksExecuted)
+  def incTaskCount(taskCount: Ref[IO, Int]) =
+    taskCount.update(_ + 1)
 
-  class SlowTestTaskHandler(tasksExecuted: Ref[IO, Int])
-      extends TestTaskHandler(tasksExecuted):
-    override def execute(task: Task): IO[Unit] =
-      IO.sleep(3.seconds) *> super.execute(task)
-  object SlowTestTaskHandler:
-    def make: IO[TestTaskHandler] = Ref.of[IO, Int](0).map: tasksExecuted =>
-      new SlowTestTaskHandler(tasksExecuted)
-
-  val task = Task.ScanTask(ProjectScanConfig(
-    "123",
-    "my-test-project",
-    List.empty,
-    false,
-    "master"
-  ))
+  def slowIncTaskCount(taskCount: Ref[IO, Int]) =
+    IO.sleep(3.seconds) *> incTaskCount(taskCount)
