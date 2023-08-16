bbc64cb2227179677d096b6c331998e4f286badc
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2023-08-16 22:15:53 +0200

    fix upkeep repository ig?

--- modules/upkeep/src/main/scala/upkeep/infra/UpkeepRepository.scala
+++ modules/upkeep/src/main/scala/upkeep/infra/UpkeepRepository.scala
@@ -12,12 +12,30 @@ import doobie.util.transactor.Transactor
 import upkeep.domain.*
 
 object UpkeepRepository:
+  /**
+   * This package (and entire module tbh) has a case of bad naming.
+   * Schemas changed and now projectId in this domain corresponds to gitlabId,
+   * but this hasn't been propagated to the entire module.
+   * TODO: Fix the naming
+   */
   def make[F[_]: MonadCancelThrow: UUIDGen](
       xa: Transactor[F]
   ): UpkeepRepository[F, String] = new:
     override def save(request: UpkeepRequest[String]): F[Unit] =
-      UUIDGen.randomUUID.flatMap: id =>
-        UpkeepRepositorySQL.insert(id, request).run.transact(xa).void
+      for
+        id <- UUIDGen.randomUUID
+        maybeProjectId <- UpkeepRepositorySQL
+          .getProjectId(request.projectId)
+          .option
+          .transact(xa)
+        _ <- maybeProjectId match
+          case None => MonadCancelThrow[F].unit
+          case Some(projectId) =>
+            UpkeepRepositorySQL.insert(id, request, projectId)
+              .run
+              .transact(xa)
+              .void
+      yield ()
 
     override def isPending(
         projectId: String,
@@ -33,26 +51,36 @@ object UpkeepRepository:
 object UpkeepRepositorySQL:
   import core.infra.persistance.sqlmappings.given
 
-  def insert(id: UUID, request: UpkeepRequest[String]): Update0 =
+  def getProjectId(gitlabId: String) =
     sql"""
-    INSERT INTO upkeepRequest (id, projectId, dependencyName, updateToVersion, url)
+    SELECT project.id
+    FROM project
+    JOIN project_scan_config ON project_scan_config.project_id = project.id
+    WHERE project_scan_config.gitlab_id = $gitlabId
+    """.query[UUID]
+
+  def insert(id: UUID, request: UpkeepRequest[?], projectId: UUID): Update0 =
+    sql"""
+    INSERT INTO upkeep_request (id, project_id, dependency_name, update_to_version, url)
     VALUES (
       $id, 
-      ${request.projectId}, 
+      ${projectId}, 
       ${request.dependencyName}, 
       ${request.updateToVersion}, 
       ${request.url})
     """.update
 
   def count(
-      projectId: String,
+      gitlabId: String,
       dependencyName: String,
       updateToVersion: String
   ): Query0[Int] =
     sql"""
     SELECT COUNT(*)
-    FROM upkeepRequest
-    WHERE projectId = $projectId
-      AND dependencyName = $dependencyName
-      AND updateToVersion = $updateToVersion
+    FROM project
+    JOIN project_scan_config ON project_scan_config.project_id = project.id
+    JOIN upkeep_request ON upkeep_request.project_id = project.id
+    WHERE project_scan_config.gitlab_id = $gitlabId
+      AND dependency_name = $dependencyName
+      AND update_to_version = $updateToVersion
     """.query[Int]
