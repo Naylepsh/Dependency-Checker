1b8756a06ad44d3869fd4acbbdd49e81a3b166f5
Naylepsh <naylepsh@gmail.com>
2023-08-17 21:09:08 +0200

    interactively spawn source inputs

--- static/stuff.js
+++ static/stuff.js
@@ -5,3 +5,22 @@ const removeParent = (element) => {
 const removeClosest = (element, selector) => {
   htmx.closest(element, selector).remove()
 }
+
+const addTxtInput = () => {
+  const template = document.querySelector('#txt-source-template')
+  useSourceTemplate(template)
+}
+
+const addTomlInput = () => {
+  const template = document.querySelector('#toml-source-template')
+  useSourceTemplate(template)
+}
+
+const useSourceTemplate = (templateElement) => {
+  const newSource = templateElement.cloneNode(true)
+  newSource.classList.remove('hidden')
+  newSource.removeAttribute('id')
+
+  const sourcesFormGroup = document.querySelector('#sources')
+  sourcesFormGroup.appendChild(newSource)
+}
