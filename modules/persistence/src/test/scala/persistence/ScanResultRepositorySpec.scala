c42b56efc8ac1e468768d201c18db46fb06c6715
Naylepsh <naylepsh@gmail.com>
2023-08-22 23:28:51 +0200

    fix tests

--- modules/persistence/src/test/scala/persistence/ScanResultRepositorySpec.scala
+++ modules/persistence/src/test/scala/persistence/ScanResultRepositorySpec.scala
@@ -27,6 +27,7 @@ import org.legogroup.woof.{ *, given }
 import core.domain.Time
 import org.scalatest.Checkpoints.Checkpoint
 import org.scalatest.Succeeded
+import ScanResultRepository.ScanResultRepositorySQL.GetAllResult
 
 class ScanResultRepositorySpec extends AsyncFreeSpec with AsyncIOSpec
     with Matchers:
@@ -65,6 +66,11 @@ class ScanResultRepositorySpec extends AsyncFreeSpec with AsyncIOSpec
         )
         cp.reportAll()
         Succeeded
+
+  "GetAllResult.toDomain constructs a proper report" in:
+    val scanReports = GetAllResult.toDomain(testGetAllResults)
+    scanReports should contain only (expectedFirstProjectReport, expectedSecondProjectReprort)
+
 
 object ScanResultRepositorySpec:
   val transactor = Resource.eval(AppConfig.load[IO]).flatMap: config =>
@@ -106,3 +112,74 @@ object ScanResultRepositorySpec:
   val noop: Output[IO] = new:
     override def output(str: String): IO[Unit]      = IO.unit
     override def outputError(str: String): IO[Unit] = IO.unit
+
+  val now = DateTime.now()
+  val expectedFirstProjectReport = ScanReport(
+    projectName = "first-project",
+    dependenciesReports = List(
+      Grouped(
+        groupName = "requirements.txt",
+        items = List(DependencyReport(
+          name = "Django",
+          currentVersion = Some("1.2.3"),
+          latestVersion = "4.5.6",
+          latestReleaseDate = Some(now),
+          vulnerabilities =
+            List("first-vulnerability", "second-vulnerability"),
+          notes = Some("requires python>=4.20")
+        ))
+      )
+    )
+  )
+  val expectedSecondProjectReprort = ScanReport(
+    projectName = "second-project",
+    dependenciesReports = List(
+      Grouped(
+        groupName = "requirements.txt",
+        items = List(DependencyReport(
+          name = "Flask",
+          currentVersion = Some("2.3.4"),
+          latestVersion = "2.3.5",
+          latestReleaseDate = Some(now),
+          vulnerabilities = List.empty,
+          notes = None
+        ))
+      )
+    )
+  )
+
+  val testGetAllResults = List(
+    GetAllResult(
+      projectName = "first-project",
+      groupName = "requirements.txt",
+      dependencyId = "1",
+      dependencyName = "Django",
+      dependencyCurrentVersion = Some("1.2.3"),
+      dependencyLatestVersion = "4.5.6",
+      dependencyLatestReleaseDate = Some(now),
+      dependencyNotes = Some("requires python>=4.20"),
+      dependencyVulnerability = Some("first-vulnerability")
+    ),
+    GetAllResult(
+      projectName = "first-project",
+      groupName = "requirements.txt",
+      dependencyId = "1",
+      dependencyName = "Django",
+      dependencyCurrentVersion = Some("1.2.3"),
+      dependencyLatestVersion = "4.5.6",
+      dependencyLatestReleaseDate = Some(now),
+      dependencyNotes = Some("requires python>=4.20"),
+      dependencyVulnerability = Some("second-vulnerability")
+    ),
+    GetAllResult(
+      projectName = "second-project",
+      groupName = "requirements.txt",
+      dependencyId = "1",
+      dependencyName = "Flask",
+      dependencyCurrentVersion = Some("2.3.4"),
+      dependencyLatestVersion = "2.3.5",
+      dependencyLatestReleaseDate = Some(now),
+      dependencyNotes = None,
+      dependencyVulnerability = None
+    )
+  )
