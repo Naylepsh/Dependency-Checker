9c59854f14bd6c8aae50361d4a3d0f0356d2862b
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2023-08-23 20:09:30 +0200

    speedup processor

--- modules/processor/src/main/scala/processor/TaskProcessor.scala
+++ modules/processor/src/main/scala/processor/TaskProcessor.scala
@@ -6,71 +6,71 @@ import cats.effect.{ Ref, Resource, Spawn, Temporal }
 import cats.Monad
 import cats.syntax.all.*
 import concurrent.duration.*
+import org.legogroup.woof.{ *, given }
 
 private type Task[F[_]] = () => F[Unit]
 
 object TaskProcessor:
 
-  def make[F[_]: Monad: Temporal](
+  def make[F[_]: Monad: Temporal: Logger](
       maxWorkers: Int,
       stopPredicate: F[Boolean],
       mainWorkerSleepTime: Option[Duration]
   ): Resource[F, TaskProcessor[F]] =
     Supervisor[F](await = true).evalMap: supervisor =>
       for
-        queue          <- Queue.unbounded[F, Task[F]]
-        tasksInProgess <- Ref.of[F, Int](0)
-        mainWorker =
-          TaskWorker(queue, tasksInProgess, mainWorkerSleepTime)
+        queue       <- Queue.unbounded[F, Task[F]]
+        workerCount <- Ref.of[F, Int](1)
+        mainWorker = TaskWorker(queue)
         processor = TaskProcessor.make(
           supervisor,
           queue,
-          tasksInProgess,
+          workerCount,
           maxWorkers
         )
         _ <- supervisor.supervise(
-          mainWorker.runOnce.whileM_(stopPredicate.map(!_))
+          mainWorker.runOnce(mainWorkerSleepTime).whileM_(stopPredicate.map(!_))
         )
       yield processor
 
-  private def make[F[_]: Monad: Temporal](
+  private def make[F[_]: Monad: Temporal: Logger](
       supervisor: Supervisor[F],
       queue: Queue[F, Task[F]],
-      tasksInProgress: Ref[F, Int],
+      workerCount: Ref[F, Int],
       maxWorkers: Int
   ): TaskProcessor[F] = new:
     def add(task: => F[Unit]): F[Unit] =
       for
-        _               <- queue.offer(() => task)
-        inProgressCount <- tasksInProgress.get
-        _               <-
-          /**
-           * TODO: There's an edge case here:
-           *  If a user adds the jobs quickly:
-           *  `processor.add(task1) *> processor.add(task2) *> processor.add(task3)`
-           *  then there's a chance that inProgressCount will still be 0
-           *  by the time of `add(task2)` and `add(task3)`
-           *  preventing the additional workers from spawning
-           */
-          if 0 < inProgressCount && inProgressCount <= maxWorkers
+        _            <- Logger[F].debug("Adding a task on the queue")
+        _            <- queue.offer(() => task)
+        tasksOnQueue <- queue.size
+        busyWorkers  <- workerCount.get
+        _ <-
+          if 0 < tasksOnQueue && busyWorkers < maxWorkers
           then
-            val worker = TaskWorker(queue, tasksInProgress)
-            supervisor.supervise(worker.runOnce).void
-          else Monad[F].unit
+            val worker = TaskWorker(queue)
+            Logger[F].debug(s"Adding a new worker (to existing $busyWorkers)")
+              *> workerCount.update(_ + 1)
+              *> supervisor.supervise(
+                worker.runWhileQueueIsNotEmpty *> workerCount.update(_ - 1)
+              ).void
+          else
+            Logger[F].debug(
+              s"Keeping the worker count the same (at $busyWorkers)"
+            )
       yield ()
 
-private class TaskWorker[F[_]: Monad: Temporal](
-    queue: Queue[F, Task[F]],
-    tasksInProgress: Ref[F, Int],
-    sleepOnEmpty: Option[Duration] = None
-):
-  def runOnce: F[Unit] =
+private class TaskWorker[F[_]: Monad: Temporal](queue: Queue[F, Task[F]]):
+  def runOnce(sleepOnEmpty: Option[Duration]): F[Unit] =
     queue.tryTake.flatMap:
       case None =>
         sleepOnEmpty
           .map(Temporal[F].sleep)
           .getOrElse(Monad[F].unit)
       case Some(task) =>
-        tasksInProgress.update(_ + 1)
-          *> task()
-          *> tasksInProgress.update(_ - 1)
+        task()
+
+  def runWhileQueueIsNotEmpty: F[Unit] =
+    queue.tryTake.flatMap:
+      case None       => Monad[F].unit
+      case Some(task) => task() *> runWhileQueueIsNotEmpty
