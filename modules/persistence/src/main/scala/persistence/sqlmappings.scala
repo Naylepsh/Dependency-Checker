330036e7351367b19b868a339aba6545016d10cb
Naylepsh <naylepsh@gmail.com>
2023-08-26 22:33:27 +0200

    fix non-symmetrical datetime handling

--- modules/persistence/src/main/scala/persistence/sqlmappings.scala
+++ modules/persistence/src/main/scala/persistence/sqlmappings.scala
@@ -6,10 +6,16 @@ import java.util.UUID
 import doobie.*
 import doobie.implicits.javatimedrivernative.*
 import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 object sqlmappings:
-  given Get[UUID]     = Get[String].map(UUID.fromString)
-  given Put[UUID]     = Put[String].contramap(_.toString)
-  given Get[DateTime] = Get[String].map(DateTime.parse)
-  given Put[DateTime] = Put[Instant]
-    .contramap(dt => Instant.ofEpochMilli(dt.getMillis()))
+  given Get[UUID] = Get[String].map(UUID.fromString)
+  given Put[UUID] = Put[String].contramap(_.toString)
+
+  private val formatter =
+    DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+  given Get[DateTime] = Get[String].map: str =>
+    formatter.parseDateTime(str)
+  given Put[DateTime] = Put[String]
+    .contramap: dt =>
+      formatter.print(dt)
