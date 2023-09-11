b6acb6cf6c8c50c98542e7522598481e8c4472eb
Naylepsh <naylepsh@gmail.com>
2023-09-11 16:03:58 +0200

    chore: remove unused scan report export service

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,7 @@ import upkeep.application.UpkeepCli
 object Main extends CommandIOApp(
       name = "ganyu",
       header = "",
-      version = "0.9.2"
+      version = "0.9.3"
     ):
 
   def main: Opts[IO[ExitCode]] =
