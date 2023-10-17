1dd7989fe5c620c8b08a1005e866e3339df21fd5
naylepsh <naylepsh@gmail.com>
2023-10-17 17:22:19 +0200

    tests: fix imports in tests

--- modules/advisory/src/test/scala/advisory/GithubAdvisorySpec.scala
+++ modules/advisory/src/test/scala/advisory/GithubAdvisorySpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.should.Matchers
 import cats.effect.IO
 import cats.effect.testing.scalatest.AsyncIOSpec
-import core.domain.project.VulnerabilitySeverity
+import core.domain.vulnerability.VulnerabilitySeverity
 
 class GithubAdvisorySpec extends AsyncFreeSpec with AsyncIOSpec
     with Matchers:
