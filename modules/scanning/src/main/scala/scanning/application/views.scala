0481b0d4d33cf194bdf4cc12a57062f227ba2c4c
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2023-10-25 14:52:29 +0200

    feat: add favicon

--- modules/scanning/src/main/scala/scanning/application/views.scala
+++ modules/scanning/src/main/scala/scanning/application/views.scala
@@ -14,6 +14,11 @@ object views:
         link(
           href := "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css",
           rel  := "stylesheet"
+        ),
+        link(
+          rel    := "icon",
+          `type` := "image/x-icon",
+          href   := "/static/favicon.ico"
         )
       ),
       body(
