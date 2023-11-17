3b73d6a178c71e3ba441f65a72390df2d7af79cc
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2023-11-17 16:24:50 +0100

    feat: finish jira modula

---
+++ modules/jira/src/main/scala/jira/Main.scala
@@ -0,0 +1,24 @@
+package jira
+
+import cats.effect.*
+import cats.syntax.all.*
+import sttp.client3.httpclient.cats.HttpClientCatsBackend
+
+object Main extends IOApp:
+  def run(args: List[String]): IO[ExitCode] =
+    val config = Config(
+      username = Username("???"),
+      password = Password("???"),
+      address = Address("???")
+    )
+    HttpClientCatsBackend.resource[IO]().use: backend =>
+      val jira        = Jira.make[IO](config, backend)
+      val key         = ProjectKey("OPDDEV")
+      val summary     = Summary("Summary test")
+      val description = Description("Description test")
+      val issueType   = "Task"
+      jira
+        .createTicket(key, summary, description, issueType)
+        .map: result =>
+          println(s"result: $result")
+        .as(ExitCode.Success)
