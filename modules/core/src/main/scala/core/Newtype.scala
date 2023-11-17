8309943fe2a1753cdbf2c29fe457d542b58d7d64
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2023-11-17 16:39:38 +0100

    chore: add Encoder and Decoder instances to Newtype

--- modules/core/src/main/scala/core/Newtype.scala
+++ modules/core/src/main/scala/core/Newtype.scala
@@ -1,8 +1,13 @@
 package core
 
 import cats.kernel.Order
+import io.circe.{ Decoder, Encoder }
 
-abstract class Newtype[A](using ord: Order[A]):
+abstract class Newtype[A](
+    using ord: Order[A],
+    enc: Encoder[A],
+    dec: Decoder[A]
+):
   /**
    * Shamelessly copy-pasted from:
    * https://github.com/gvolpe/trading/blob/main/modules/domain/shared/src/main/scala/trading/Newtype.scala
@@ -15,4 +20,6 @@ abstract class Newtype[A](using ord: Order[A]):
 
   extension (t: Type) inline def value: A = t
 
-  given Order[Type] = ord
+  given Order[Type]   = ord
+  given Encoder[Type] = enc
+  given Decoder[Type] = dec
