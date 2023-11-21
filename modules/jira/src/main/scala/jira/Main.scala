d2cea98936f16b84bc4d25450ddc81f34706c1a4
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2023-11-21 21:07:39 +0100

    fix: correct jira templating

--- modules/jira/src/main/scala/jira/Main.scala
+++ modules/jira/src/main/scala/jira/Main.scala
@@ -7,16 +7,30 @@ import sttp.client3.httpclient.cats.HttpClientCatsBackend
 object Main extends IOApp:
   def run(args: List[String]): IO[ExitCode] =
     val config = Config(
-      username = Username("???"),
-      password = Password("???"),
-      address = Address("???")
+      username = Username(???),
+      password = Password(???),
+      address = Address(???)
     )
     HttpClientCatsBackend.resource[IO]().use: backend =>
-      val jira      = Jira.make[IO](config, backend, Template(""))
-      val key       = ProjectKey("OPDDEV")
+      val jira = Jira.make[IO](
+        config,
+        backend,
+        Template
+          .fromFile("./jira-template.example.json")
+          .right
+          .getOrElse(Template("{}"))
+      )
+      val key       = ProjectKey(???)
       val issueType = "Task"
+      val variables = Map(
+        "projectName"     -> "test-project",
+        "dependencyName"  -> "test-dependency",
+        "fromVersion"     -> "1.2.3",
+        "toVersion"       -> "1.2.4",
+        "mergeRequestUrl" -> "http://localhost:8000/path/to/test-project/-/merge_requests/42"
+      )
       jira
-        .createTicket(key, issueType, Map())
+        .createTicket(key, issueType, variables)
         .map: result =>
           println(s"result: $result")
         .as(ExitCode.Success)
