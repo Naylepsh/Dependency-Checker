cbdeda4a377448fd22d26c70f1fd452ef2202a6a
naylepsh <naylepsh@gmail.com>
2023-11-27 16:06:16 +0100

    feat: add healtcheck route

--- src/main/scala/controllers.scala
+++ src/main/scala/controllers.scala
@@ -1,31 +1,28 @@
 import cats.data.{ Kleisli, OptionT }
 import cats.effect.kernel.Sync
 import cats.syntax.all.*
-import cats.{Monad, MonadError, MonadThrow}
+import cats.{ Monad, MonadError, MonadThrow }
 import core.controller.Controller
 import fs2.io.file.Files
 import org.http4s.*
 import org.http4s.dsl.Http4sDsl
 import org.http4s.headers.*
+import org.http4s.server.Router
 import org.http4s.syntax.all.*
 import org.legogroup.woof.{ *, given }
 
 object controllers:
   object RootController:
-    type ThrowableMonadError[F[_]] = MonadError[F, Throwable]
-
-    def make[F[_]: Monad: ThrowableMonadError: Sync: Files]: Controller[F] =
+    def make[F[_]: MonadThrow: Sync: Files]: Controller[F] =
       new Controller[F] with Http4sDsl[F]:
-        def routes: HttpRoutes[F] = HttpRoutes.of[F]:
+        val routes: HttpRoutes[F] = HttpRoutes.of[F]:
           case GET -> Root =>
             TemporaryRedirect(Location(uri"/project"))
 
   object StaticFileController:
-    type ThrowableMonadError[F[_]] = MonadError[F, Throwable]
-
-    def make[F[_]: Monad: ThrowableMonadError: Sync: Files]: Controller[F] =
+    def make[F[_]: MonadThrow: Sync: Files]: Controller[F] =
       new Controller[F] with Http4sDsl[F]:
-        def routes: HttpRoutes[F] = HttpRoutes.of[F]:
+        val routes: HttpRoutes[F] = HttpRoutes.of[F]:
           case request @ GET -> Root / "static" / path =>
             val p = fs2.io.file.Path(s"./static/$path")
             StaticFile.fromPath(p, Some(request)).getOrElseF(NotFound())
@@ -48,3 +45,11 @@ object controllers:
             _ <- OptionT.liftF:
               Logger[F].info(s"${req.method} ${req.uri} - ${res.status}")
           yield res
+
+  object HealthcheckController:
+    def make[F[_]: MonadThrow]: Controller[F] =
+      new Controller[F] with Http4sDsl[F]:
+        val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F]:
+          case GET -> Root => Ok()
+
+        val routes: HttpRoutes[F] = Router("api/v1/healtcheck" -> httpRoutes)
